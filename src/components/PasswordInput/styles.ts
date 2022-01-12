7d8a9b7ee714ebf325cef3030acf2d63e5121240
Hariel Santana <hariol.santana138@gmail.com>
2022-01-12 10:10:40 -0300

    feat(input): adicionando inputs na tela de signin

--- src/components/PasswordInput/styles.ts
+++ src/components/PasswordInput/styles.ts
@@ -1,13 +1,17 @@
 import { TextInput } from "react-native";
 import { BorderlessButton } from "react-native-gesture-handler";
 import { RFValue } from "react-native-responsive-fontsize";
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
+
+interface Props {
+  isFocused: boolean;
+}
 
 export const Container = styled.View`
   flex-direction: row;
 `;
 
-export const IconContainer = styled.View`
+export const IconContainer = styled.View<Props>`
   height: 56px;
   width: 55px;
   justify-content: center;
@@ -16,9 +20,16 @@ export const IconContainer = styled.View`
   margin-right: 2px;
 
   background-color: ${({ theme }) => theme.colors.background_secondary};
+
+  ${({ isFocused, theme }) =>
+    isFocused &&
+    css`
+      border-bottom-width: 2px;
+      border-bottom-color: ${theme.colors.main};
+    `}
 `;
 
-export const InputText = styled(TextInput)`
+export const InputText = styled(TextInput)<Props>`
   flex: 1;
 
   background-color: ${({ theme }) => theme.colors.background_secondary};
@@ -27,4 +38,11 @@ export const InputText = styled(TextInput)`
   font-size: ${RFValue(15)}px;
 
   padding: 0 23px;
+
+  ${({ isFocused, theme }) =>
+    isFocused &&
+    css`
+      border-bottom-width: 2px;
+      border-bottom-color: ${theme.colors.main};
+    `}
 `;
